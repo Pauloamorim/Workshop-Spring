55e4e7de388d8262564ad02ce72688c1ba624841
Paulo <paulovitor.10@hotmail.com>
2016-01-10 21:28:51 -0200

    -Método salvar/-Criação de repositório/-Alteração de linguagem

---
+++ src/main/java/com/algaworks/cobranca/enums/StatusTituloEnum.java
@@ -0,0 +1,18 @@
+package com.algaworks.cobranca.enums;
+
+public enum StatusTituloEnum {
+
+	PENDENTE("Pendente"),
+	RECEBIDO("Recebido");
+	
+	private String descricao;
+	
+	private StatusTituloEnum(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+}
