2c29b6da3f21e0d60d1c911cc45240491d2295d3
Paulo <paulovitor.10@hotmail.com>
2016-01-11 20:57:29 -0200

    Configurando mysql e criando combo dinâmica

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,11 +1,16 @@
 package com.algaworks.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.cobranca.enums.StatusTituloEnum;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
 
@@ -18,8 +23,9 @@ public class TituloController {
 	
 
 	@RequestMapping("/novo")
-	public String novo(){
-		return "CadastroTitulo";
+	public ModelAndView novo(){
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
@@ -28,6 +34,11 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem","Título salvo com sucesso");
 		return mv;
+	}
+	
+	@ModelAttribute("todosStatusTitulos")
+	public List<StatusTituloEnum> todosStatusTitulo(){
+		return Arrays.asList(StatusTituloEnum.values());
 	}
 	
 	
