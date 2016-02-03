00fa20b7ca68798714f06faba7863d07a428eba8
Paulo <paulovitor.10@hotmail.com>
2016-02-03 18:58:04 -0200

    commit 3 aulas

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -5,10 +5,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.cobranca.enums.StatusTituloEnum;
 import com.algaworks.cobranca.model.Titulo;
@@ -18,22 +22,28 @@ import com.algaworks.cobranca.repository.Titulos;
 @RequestMapping(value="/titulos")
 public class TituloController {
 	
+	
+	private String CADASTRO_TITULO="CadastroTitulo";
+	
 	@Autowired
 	private Titulos titulos;
 	
 
 	@RequestMapping("/novo")
 	public ModelAndView novo(){
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_TITULO);
+		mv.addObject(new Titulo());
 		return mv;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo){
+	public String salvar(@Validated Titulo titulo,Errors errors,RedirectAttributes attributes){
+		if(errors.hasErrors()){
+			return "CadastroTitulo";
+		}
 		titulos.save(titulo);
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem","Título salvo com sucesso");
-		return mv;
+		attributes.addFlashAttribute("mensagem","Título salvo com sucesso");
+		return "redirect:/titulos/novo";
 	}
 	
 	@RequestMapping
@@ -41,6 +51,14 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		List<Titulo> todosTitulos =  titulos.findAll();
 		mv.addObject("titulos",todosTitulos);
+		return mv;
+	}
+	
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable Long codigo){
+		Titulo titulo = titulos.findOne(codigo);
+		ModelAndView mv = new ModelAndView(CADASTRO_TITULO);
+		mv.addObject(titulo);
 		return mv;
 	}
 	
--- src/main/java/com/algaworks/cobranca/model/Titulo.java
+++ src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -12,9 +12,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
+import org.springframework.jmx.export.annotation.ManagedNotifications;
 
 import com.algaworks.cobranca.enums.StatusTituloEnum;
 
@@ -25,12 +31,18 @@ public class Titulo {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotEmpty(message="Descrição é obrigatória")
+	@Size(max=60,message="A descrição não pode conter mais de 60 caracteres")
 	private String descricao;
 	
+	@NotNull(message="Data de vencimento é obrigatória")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern="dd/MM/yyyy")
 	private Date dataVencimento;
 	
+	@DecimalMin(value="0.01",message="Valor não pode ser menor que 0.01")
+	@DecimalMax(value="9999999.99",message="Valor não pode ser maior que 9,999,999.99")
+	@NotNull(message="Valor é obrigatória")
 	@NumberFormat(pattern="#,##0.00")
 	private BigDecimal valor;
 	
